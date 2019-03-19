c89ea52bd7da2598d5e32d51d3e6514be63009e9
Gabriel Bhering <gbhering@protonmail.ch>
2019-03-19 14:58:43 -0300

    changed db to v to match class notes, and implemented the pretty parens thing

--- basic_implementation.py
+++ basic_implementation.py
@@ -5,70 +5,70 @@ def findParensMatch(F,i):
 		if v == '(':s+=1
 		if s == 0: return i+j
 
-def solve(F, db):
-	db[True] = True
-	db[False] = False
+def solve(F, v):
+	v[True] = True
+	v[False] = False
 
-	F = F.split(' ')
-	return phi(F, db)
+	F = F.replace('(','( ').replace(')', ' )').split()
+	return phi(F, v)
 
 
 # order of cases is also order of priority
-def phi(F, db):
+def phi(F, v):
 	if len(F) > 2 and '(' in F:
 		i = F.index('(')
 		j = findParensMatch(F,i)
-		return phi(F[:i] + [phi(F[i+1:j],db)] + F[j+1:], db)
+		return phi(F[:i] + [phi(F[i+1:j],v)] + F[j+1:], v)
 
 	if len(F) > 2 and "NOT" in F:
 		i = F.index('NOT')
-		return phi(F[:i] + [phi(F[i:i+2], db)] + F[i+2:], db)
+		return phi(F[:i] + [phi(F[i:i+2], v)] + F[i+2:], v)
 
 	if len(F) > 3 and "AND" in F:
 		i = F.index('AND')
-		return phi(F[:i-1] + [phi(F[i-1:i+2], db)] + F[i+2:], db)
+		return phi(F[:i-1] + [phi(F[i-1:i+2], v)] + F[i+2:], v)
 
 	if len(F) > 3 and "OR" in F:
 		i = F.index('OR')
-		return phi(F[:i-1] + [phi(F[i-1:i+2], db)] + F[i+2:], db)
+		return phi(F[:i-1] + [phi(F[i-1:i+2], v)] + F[i+2:], v)
 
 	if F[0] == "NOT":
-		return not db[F[1]];
+		return not v[F[1]];
 
 	if F[1] == "OR":
-		return db[F[0]] or db[F[2]]
+		return v[F[0]] or v[F[2]]
 
 	if F[1] == "AND":
-		return db[F[0]] and db[F[2]]
+		return v[F[0]] and v[F[2]]
 
 	return F
 
 
 
 if __name__=='__main__':
-	db = { 'A': True, 'B': False, 'C': False }
-	print("DB:", db)
+	v = { 'A': True, 'B': False, 'C': False }
+	print("v:", v)
 
 	F = "A AND B"
-	print(F, '=>', solve(F, db))
+	print(F, '=>', solve(F, v))
 
 	F = "A OR B"
-	print(F, '=>', solve(F, db))
+	print(F, '=>', solve(F, v))
 
 	F = "NOT A"
-	print(F, '=>', solve(F, db))
+	print(F, '=>', solve(F, v))
 
 	F = "A OR NOT B OR C"
-	print(F, '=>', solve(F, db))
+	print(F, '=>', solve(F, v))
 
 	F = "NOT A AND NOT B OR NOT C"
-	print(F, '=>', solve(F, db))
+	print(F, '=>', solve(F, v))
 
 	F = "NOT A OR B OR C"
-	print(F, '=>', solve(F, db))
+	print(F, '=>', solve(F, v))
 
-	F = "NOT ( A OR B OR C )"
-	print(F, '=>', solve(F, db))
+	F = "NOT (A OR B OR C)"
+	print(F, '=>', solve(F, v))
 
-	F = "NOT ( A OR B OR C ) OR A"
-	print(F, '=>', solve(F, db))
+	F = "NOT (A OR B OR C) OR A"
+	print(F, '=>', solve(F, v))
