3c0a751e04c7b14500fcb0ef99e27e1adfd3fde4
Gabriel Bhering <git@gbhering.dev>
2019-03-20 08:10:23 -0300

    Starting on modal

---
+++ modal_implementation.py
@@ -0,0 +1,92 @@
+def findParensMatch(f,i):
+	s = 0
+	for j,v in enumerate(f[i:]):
+		if v == ')': s-=1
+		if v == '(':s+=1
+		if s == 0: return i+j
+
+def solve(f, v):
+	v[True] = True
+	v[False] = False
+
+	f = f.replace('(','( ').replace(')', ' )').split()
+	return phi(f, v)
+
+
+# order of cases is also order of priority
+def phi(f, v):
+	# base cases
+	if len(f)==2 and f[0]=="NOT":
+		return not v[f[1]];
+	if len(f)==3 and f[1] == "AND":
+		return v[f[0]] and v[f[2]]
+	if len(f)==3 and f[1]=="OR":
+		return v[f[0]] or v[f[2]]
+	if len(f)==3 and f[1]=="->":
+		return phi([ 'NOT', v[f[0]], 'OR', v[f[2]] ], v)
+
+	if len(f) > 2 and '(' in f:
+		i = f.index('(')
+		j = findParensMatch(f,i)
+		return phi(f[:i] + [phi(f[i+1:j],v)] + f[j+1:], v)
+
+	if len(f) > 2 and "NOT" in f:
+		i = f.index('NOT')
+		return phi(f[:i] + [phi(f[i:i+2], v)] + f[i+2:], v)
+
+	if len(f) > 3 and "AND" in f:
+		i = f.index('AND')
+		return phi(f[:i-1] + [phi(f[i-1:i+2], v)] + f[i+2:], v)
+
+	if len(f) > 3 and "OR" in f:
+		i = f.index('OR')
+		return phi(f[:i-1] + [phi(f[i-1:i+2], v)] + f[i+2:], v)
+
+	if len(f) > 3 and "->" in f:
+		i = f.index('->')
+		return phi(f[:i-1] + [phi(f[i-1:i+2], v)] + f[i+2:], v)
+
+	# things have failed if we get here
+	return f
+
+
+
+if __name__=='__main__':
+	v = { 'A': True, 'B': False, 'C': False }
+	print("v:", v)
+
+	f = "A AND B"
+	print(f, '=>', solve(f, v))
+
+	f = "A OR B"
+	print(f, '=>', solve(f, v))
+
+	f = "NOT A"
+	print(f, '=>', solve(f, v))
+
+	f = "A OR NOT B OR C"
+	print(f, '=>', solve(f, v))
+
+	f = "NOT A AND NOT B OR NOT C"
+	print(f, '=>', solve(f, v))
+
+	f = "NOT A OR B OR C"
+	print(f, '=>', solve(f, v))
+
+	f = "NOT (A OR B OR C)"
+	print(f, '=>', solve(f, v))
+
+	f = "NOT (A OR B OR C) OR A"
+	print(f, '=>', solve(f, v))
+
+	f = "(A -> C)"
+	print(f, '=>', solve(f, v))
+
+	f = "(C -> A)"
+	print(f, '=>', solve(f, v))
+
+	f = "(A -> A)"
+	print(f, '=>', solve(f, v))
+
+	f = "(B -> C)"
+	print(f, '=>', solve(f, v))
