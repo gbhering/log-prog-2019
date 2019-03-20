d73e26f79b3ba4a2530c8106a13c2835366c3580
Gabriel Bhering <git@gbhering.dev>
2019-03-20 03:06:42 -0300

    little style patches and the implies function

--- basic_implementation.py
+++ basic_implementation.py
@@ -15,6 +15,16 @@ def solve(F, v):
 
 # order of cases is also order of priority
 def phi(F, v):
+	# base cases
+	if len(F)==2 and F[0]=="NOT":
+		return not v[F[1]];
+	if len(F)==3 and F[1] == "AND":
+		return v[F[0]] and v[F[2]]
+	if len(F)==3 and F[1]=="OR":
+		return v[F[0]] or v[F[2]]
+	if len(F)==3 and F[1]=="->":
+		return phi([ 'NOT', v[F[0]], 'OR', v[F[2]] ], v)
+
 	if len(F) > 2 and '(' in F:
 		i = F.index('(')
 		j = findParensMatch(F,i)
@@ -32,15 +42,11 @@ def phi(F, v):
 		i = F.index('OR')
 		return phi(F[:i-1] + [phi(F[i-1:i+2], v)] + F[i+2:], v)
 
-	if F[0] == "NOT":
-		return not v[F[1]];
+	if len(F) > 3 and "->" in F:
+		i = F.index('->')
+		return phi(F[:i-1] + [phi(F[i-1:i+2], v)] + F[i+2:], v)
 
-	if F[1] == "OR":
-		return v[F[0]] or v[F[2]]
-
-	if F[1] == "AND":
-		return v[F[0]] and v[F[2]]
-
+	# things have failed if we get here
 	return F
 
 
@@ -71,4 +77,16 @@ if __name__=='__main__':
 	print(F, '=>', solve(F, v))
 
 	F = "NOT (A OR B OR C) OR A"
+	print(F, '=>', solve(F, v))
+
+	F = "(A -> C)"
+	print(F, '=>', solve(F, v))
+
+	F = "(C -> A)"
+	print(F, '=>', solve(F, v))
+
+	F = "(A -> A)"
+	print(F, '=>', solve(F, v))
+
+	F = "(B -> C)"
 	print(F, '=>', solve(F, v))
