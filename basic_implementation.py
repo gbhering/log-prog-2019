ac9e0a61c09d124ac51b1ad73383ec0105b11c97
Gabriel Bhering <gbhering@protonmail.ch>
2019-03-21 16:44:04 -0300

    bumped up NOT to max priority

--- basic_implementation.py
+++ basic_implementation.py
@@ -21,6 +21,10 @@ def rec(phi, V):
 		raise SyntaxError('Malformed expression!', phi)
 	if len(phi)==1:
 		return phi[0] if phi[0] in [True, False] else V[phi[0]]
+
+	# the actual recursion
+	if "NOT" == phi[0]:
+		return not rec( phi[1:], V )
 	if '(' in phi:
 		i, j = findParens(phi)
 		return rec( phi[:i] + [ rec( phi[i+1:j], V ) ] + phi[j+1:], V )
@@ -33,8 +37,6 @@ def rec(phi, V):
 	if "->" in phi:
 		i = phi.index('->')
 		return not rec( phi[:i], V ) or rec( phi[i+1:], V )
-	if "NOT" == phi[0]:
-		return not rec( phi[1], V )
 
 	# things haVe failed if we get here
 	return phi
