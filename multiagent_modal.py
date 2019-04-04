3d183b4f6d171c17472b18720cdcba481aac4f1e
Gabriel Bhering <gbhering@protonmail.ch>
2019-04-04 17:41:42 -0300

    rewriting and styling for legibility

--- multiagent_modal.py
+++ multiagent_modal.py
@@ -37,31 +37,20 @@ def satSquare( phi, W, R, V, w ):
 	return True
 
 def sat( phi, W, R, V, w ):
-	if phi in [True, False]:
-		return phi
-	if len(phi)==0:
-		raise SyntaxError('Malformed expression!', phi)
-	if len(phi)==1:
-		if phi[0] in V:
-			return True if w in V[phi[0]] else False
-		if  phi[0] == '0':
-			return False
-		if  phi[0] == True or phi[0] == False:
-			return phi[0]
+	# base cases
+	if phi == True or phi == [True]: return True
+	if phi == False or phi == [False]: return False
+	if len(phi)==0: raise SyntaxError('Malformed expression!', phi)
+	if len(phi)==1 and phi[0] in V:	return ( w in V[phi[0]] )
 	
-	if "¬" == phi[0]:
-		return not sat( phi[1:], W, R, V, w)
-	if "◇" == phi[0]:
-		for _w in R[w]:
-			if sat( phi[1:], W, R, V, _w ): return True
-		return False
-	if "□" == phi[0]:
-		for _w in R[w]:
-			if not sat( phi[1:], W, R, V, _w ): return False
-		return True
+	if "¬" == phi[0]: return not sat( phi[1:], W, R, V, w )
+	if "<" == phi[0]: return satDiamond( phi, W, R, V, w )
+	if "[" == phi[0]: return satSquare( phi, W, R, V, w )
+	
 	if '(' in phi:
 		i, j = findParens(phi)
-		return sat( phi[:i] + [ sat( phi[i+1:j], W, R, V, w) ] + phi[j+1:], W, R, V, w)
+		return sat( phi[:i] + [ sat( phi[i+1:j], W, R, V, w ) ] + phi[j+1:], W, R, V, w )
+	
 	if "∧" in phi:
 		i = phi.index('∧')
 		return sat( phi[:i], W, R, V, w) and sat( phi[i+1:], W, R, V, w)
