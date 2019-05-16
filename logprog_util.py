f90be5ed559e15db8d6fc4689b7449d5799a8f9d
Gabriel Bhering <gbhering@protonmail.ch>
2019-05-16 15:39:38 -0300

    standardizing implementations

--- logprog_util.py
+++ logprog_util.py
@@ -3,6 +3,6 @@ def findParens(phi, st='(', end=')'):
 	s = 0
 	for j in range(i, len(phi)+1):
 		if phi[j] == end: s-=1
-		if phi[j] == st: s+=1
+		elif phi[j] == st: s+=1
 		if s == 0: return i, j
 	raise SyntaxError('Unmatched parens!', st, end, phi)
